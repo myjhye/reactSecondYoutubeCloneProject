e08d322489780d68b191963a6130b346835afe1d
myjhye <t01087633799@gmail.com>
2023-09-10 16:58:13 +0900

    비디오 검색 기능(searchFeed) 완료

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,25 @@
 import { IconButton, Paper } from "@mui/material";
 import Search from '@mui/icons-material/Search';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar() {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+
+    const onhandleSubmit = (e) => {
+        e.preventDefault();
+
+        if(searchTerm) {
+            navigate(`/search/${searchTerm}`);
+        }
+    }
+
     return (
         <Paper
             component='form'
+            onSubmit={onhandleSubmit}
             sx={{
                 borderRadius: 20,
                 border: '1px solid #e3e3e3',
@@ -16,8 +31,8 @@ export default function SearchBar() {
             <input
                 className='search-bar'
                 placeholder='검색'
-                value=''
-                onChange={() => {}}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
             />
             <IconButton type='submit' sx={{ p: '10px', color: 'red' }}>
                 <Search />
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,26 @@
+import { Box, Stack, Typography } from "@mui/material";
+import { Videos } from './';
+import { useEffect, useState } from "react";
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { useParams } from "react-router-dom";
+
 export default function SearchFeed() {
+
+    const [videos, setVideos] = useState(null);
+    const { searchTerm } = useParams();
+
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+            .then((data) => setVideos(data.items))
+    }, [searchTerm]);
+
     return (
-        <div>
-            SearchFeed
-        </div>
+        <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+            {videos !== null ? ( // videos가 null이 아닌 경우에만 Videos 컴포넌트를 렌더링
+                <Videos videos={videos} />
+            ) : (
+                <Typography>Loading...</Typography> // 데이터 로딩 중에 메시지 표시
+            )}
+        </Box>
     )
 }
